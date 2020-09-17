aa95dc4b2e71580140a9fc4f072283e307129a02
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-17 15:43:00 +0545

    feat: minor header css changes in card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import './App.css';
-import { Card } from "./components/Card" 
+import React from "react";
+import "./App.css";
+import { Card } from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-     <Card />
+      <Card
+        amount="$700.00"
+        countries={[
+          { name: "United States", traffic: "3.1GB/∞" },
+          { name: "Russian Federation", traffic: "6.2GB/∞" },
+        ]}
+        ips={["178.124.100.103", "192.60.67.12"]}
+        id={7567}
+        paidTo="17.12.2018, 19:02"
+        status="ACTIVE"
+        isActive={true}
+      />
+      <Card
+        amount="$496.00"
+        countries={[
+          { name: "United States", traffic: "3.1GB/∞" },
+          { name: "China", traffic: "6.2GB/∞" },
+        ]}
+        ips={["178.124.100.103", "192.60.67.12"]}
+        id={7567}
+        paidTo="17.12.2018, 19:02"
+        status="PAUSE"
+        isActive={true}
+      />
+      <Card
+        amount="$700.00"
+        countries={[
+          { name: "Argentina", traffic: "3.1GB/∞" },
+          { name: "China", traffic: "2.97GB/∞" },
+        ]}
+        ips={["178.124.100.103", "192.60.67.12"]}
+        id={7567}
+        paidTo="17.12.2018, 19:02"
+        status="BLOCKED"
+        isActive={false}
+      />
     </div>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import React from "react";
 import "./Card.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDotCircle,
+  faPlay,
+  faPause,
+  faLock,
+  faWifi,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
 
-export const Card = () => {
-    return <h1>Here is card</h1>
+type StatusType = "ACTIVE" | "PAUSE" | "BLOCKED";
+
+interface CardProps {
+  id: number;
+  status: StatusType;
+  isActive?: boolean;
+  countries: Array<{ name: string; traffic: string }>;
+  ips: string[];
+  paidTo: string;
+  amount: string;
 }
+
+const getIcon = (status: StatusType): IconDefinition => {
+  if (status === "ACTIVE") {
+    return faPlay;
+  }
+  if (status === "PAUSE") {
+    return faPause;
+  }
+  return faLock;
+};
+
+export const Card: React.FC<CardProps> = (props) => {
+  return (
+    <div className="card">
+      <p className="heading">
+        <span>ID {props.id}</span>
+        <button className={props.status}>
+          <FontAwesomeIcon icon={getIcon(props.status)}/>
+          {props.status}
+        </button>
+        <FontAwesomeIcon icon={faDotCircle} />
+      </p>
+      <table>
+        <tr>
+          <th>COUNTRY</th>
+          <th>
+            TRAFFIC BY <span>DAY</span>
+          </th>
+        </tr>
+        {props.countries.map((c) => (
+          <tr key={c.name}>
+            <td><FontAwesomeIcon icon={faWifi} /> {c.name}</td>
+            <td>{c.traffic}</td>
+          </tr>
+        ))}
+      </table>
+      <p className="chips">
+        {props.ips.map((d) => (
+          <span key={d}>{d}</span>
+        ))}
+      </p>
+      <p className="paidTo">
+        <span>PAID TO</span>
+        <span>{props.paidTo}</span>
+        <span>{props.amount}</span>
+      </p>
+    </div>
+  );
+};
