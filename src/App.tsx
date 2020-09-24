f824cb416d1ec3ca2a719410f6cb80f9ef7349bd
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-24 13:17:48 +0545

    feat: use newly created component in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
 import React from "react";
-import "./App.css";
-import { Card } from "./components/Card";
+import "./App.scss";
+import { CardBar } from "./components/CardBar";
+import { Sidebar } from "./components/Sidebar";
+import { MainContent } from "./components/MainContent";
 
 function App() {
   return (
     <div className="App">
-      <Card
-        amount="$700.00"
-        countries={[
-          { name: "United States", traffic: "3.1 GB/∞" },
-          { name: "Russian Federation", traffic: "6.2 GB/∞" },
-        ]}
-        ips={["178.124.100.103", "192.60.67.12"]}
-        id={7567}
-        paidTo="17.12.2018, 19:02"
-        status="ACTIVE"
-        isActive={true}
-      />
-      <Card
-        amount="$496.00"
-        countries={[
-          { name: "United States", traffic: "3.1 GB/∞" },
-          { name: "China", traffic: "6.2 GB/∞" },
-        ]}
-        ips={["178.124.100.103", "192.60.67.12"]}
-        id={7567}
-        paidTo="17.12.2018, 19:02"
-        status="PAUSE"
-        isActive={false}
-      />
-      <Card
-        amount="$700.00"
-        countries={[
-          { name: "Argentina", traffic: "3.1 GB/∞" },
-          { name: "China", traffic: "2.97 GB/∞" },
-        ]}
-        ips={["178.124.100.103", "192.60.67.12"]}
-        id={7567}
-        paidTo="17.12.2018, 19:02"
-        status="BLOCKED"
-        isActive={false}
-      />
+      <Sidebar className="main-sidebar" />
+      <CardBar className="main-cardbar" />
+      <MainContent className="main-content" />
     </div>
   );
 }
