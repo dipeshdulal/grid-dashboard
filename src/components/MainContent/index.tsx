9f4bb5479f7a7bd9fc37098941d776a5e06a23e8
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-26 17:49:01 +0545

    style: heading part design added

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./main.scss";
+import { Button } from "../Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faPause } from "@fortawesome/free-solid-svg-icons";
 
 interface MainContentProps {
   className?: string;
@@ -8,7 +11,17 @@ interface MainContentProps {
 export const MainContent: React.FC<MainContentProps> = ({ className }) => {
   return (
     <div className={`content ${className || ""}`}>
-      <h1>MainContent Contents</h1>
+      <div className="content__heading">
+        <h3>Package ID 1234</h3>
+        <Button>
+          <FontAwesomeIcon icon={faPlus} />
+          Add IP's
+        </Button>
+        <Button style={{ backgroundColor: "var(--color-blue-2)" }}>
+          <FontAwesomeIcon icon={faPause} />
+          Pause
+        </Button>
+      </div>
     </div>
   );
 };
